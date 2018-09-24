1172bcd77e5a74d6f7edc231550485ab961b6a68
sccoache <sam.c.coache@gmail.com>
2018-09-23 20:59:13 -0400

    added comments

--- python-code/mongodb/masterCount.py
+++ python-code/mongodb/masterCount.py
@@ -1,7 +1,9 @@
+# Takes in a set of playlists and returns a dictionary of relevant songs based
+# on predefined properties
 def masterCount():
     # Creates a dictionary of TrackName:Score, where Score is the number of
     # occurences across all playlists
-    unique_track_scores = {}
+    unique_track_scores = {} # Dictionary of TrackName:Score
     for playlist in all_playlists:
         for track in playlist:
             if track not in unique_track_scores:
@@ -11,8 +13,8 @@ def masterCount():
 
     # Takes in the unique_track_scores dictionary
     # Creates a new list of tracks that exceed a predefined threshhold
-    unique_track_threshhold = 1
-    top_tracks = []
+    unique_track_threshhold = 1 # Number of times a track needs to appear to be considered relevant
+    top_tracks = [] # List of tracks that meet the threshhold
     for key in unique_track_scores.keys():
         if key.value() > unique_track_threshhold:
             top_tracks.append(key)
@@ -20,8 +22,8 @@ def masterCount():
             continue
 
     # From the set of playlists, creates a dictionary of PlaylistName:Score
-    # where score based on the number of top tracks that appear in the playlist. 
-    playlist_scores = {}
+    # where score based on the number of top tracks that appear in the playlist.
+    playlist_scores = {} # Dictionary of PlaylistName:Score
     for playlist in all_playlists:
         score = 0
         for track in playlist:

d4750ebb1ae39e48b4957e1655818028b05ecf92
Quinn Averill <qsaverill@wpi.edu>
2018-09-23 22:49:06 -0400

    Added logging to server

---
+++ node-server/logRequests.js
@@ -0,0 +1,26 @@
+module.exports = (req, res, next) => {
+  const getRequestDataByMethod = method => {
+    switch (method) {
+      case "GET":
+        return JSON.stringify(req.query);
+      case "POST":
+        return (
+          Array.isArray(req.body)
+            ? JSON.stringify({"numberOfItems": req.body.length})
+            : JSON.stringify(req.body)
+        );
+      default:
+        return "The server does not know what to print for this HTTP method"
+    }
+  };
+
+  const timestamp = new Date();
+  console.log('%s-%s: %s %s %s',
+    timestamp.toLocaleDateString(),
+    timestamp.toLocaleTimeString(),
+    req.method,
+    req.originalUrl,
+    getRequestDataByMethod(req.method)
+  );
+  next();
+};
--- node-server/server.js
+++ node-server/server.js
@@ -7,17 +7,7 @@ server.use(require('cors')());
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
-server.use(function(req, res, next) {
-  const timestamp = new Date();
-  console.log('%s-%s: %s %s %s',
-    timestamp.toLocaleDateString(),
-    timestamp.toLocaleTimeString(),
-    req.method,
-    req.originalUrl,
-    req.method === "GET" ? JSON.stringify(req.query) : JSON.stringify(req.body)
-  );
-  next();
-});
+server.use(require('./logRequests'));
 
 server.use('/spotify', require('./spotify'));
 server.use('/mongodb', require('./mongodb'));

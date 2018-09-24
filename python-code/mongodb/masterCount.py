88cd244c36c0382acbcc0e552cffccb7e6049b2a
sccoache <sam.c.coache@gmail.com>
2018-09-23 20:48:43 -0400

    wrapped function

--- python-code/mongodb/masterCount.py
+++ python-code/mongodb/masterCount.py
@@ -1,24 +1,27 @@
-# Gets dictionary of all unique tracks and total count of occurence
-unique_track_scores = {}
-for playlist in all_playlists:
-    for track in playlist:
-        if track not in unique_track_scores:
-            unique_track_scores[track] = 1
-        else:
-            unique_track_scores[track] = ++unique_track_scores[track]
+def masterCount():
+    # Gets dictionary of all unique tracks and total count of occurence
+    unique_track_scores = {}
+    for playlist in all_playlists:
+        for track in playlist:
+            if track not in unique_track_scores:
+                unique_track_scores[track] = 1
+            else:
+                unique_track_scores[track] = ++unique_track_scores[track]
 
-unique_track_threshhold = 1
-top_tracks = []
-for key in unique_track_scores.keys():
-    if key.value() > unique_track_threshhold:
-        top_tracks.append(key)
-    else
-        continue
+    unique_track_threshhold = 1
+    top_tracks = []
+    for key in unique_track_scores.keys():
+        if key.value() > unique_track_threshhold:
+            top_tracks.append(key)
+        else
+            continue
 
-playlist_scores = {}
-for playlist in all_playlists:
-    score = 0
-    for track in playlist:
-        if track in top_tracks:
-            ++score
-    playlist_scores[playlist] = score
+    playlist_scores = {}
+    for playlist in all_playlists:
+        score = 0
+        for track in playlist:
+            if track in top_tracks:
+                ++score
+        playlist_scores[playlist] = score
+
+    return playlist_scores

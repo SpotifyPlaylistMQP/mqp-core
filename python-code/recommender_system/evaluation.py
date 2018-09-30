998972dc299b700b6d002dad51a5dc34f227ca78
Steven McAteer <smcateer@wpi.edu>
2018-09-30 15:14:19 -0400

    fixes

--- python-code/recommender_system/evaluation.py
+++ python-code/recommender_system/evaluation.py
@@ -1,7 +1,15 @@
 # Returns a list of tracks in both recommended_tracks and original_20_percent, and their R-Precision value
-def r_precision(recommended_tracks, T, N):
+def r_precision(recommended_tracks, T, N, track_dict):
     matches = 0
+    print("---------------------------------------------------------------")
+    print("TRACKS FROM 20%")
+    for tid in T:
+        print(track_dict[tid]["name"])
+
+    print("---------------------------------------------------------------")
+    print("REC TRACKS BOI")
     for recommendation in recommended_tracks:
+        print(track_dict[recommendation]["name"])
         if recommendation in T:
             matches += 1
     return matches, matches / N
--- python-code/user_based_collaborative_filtering.py
+++ python-code/user_based_collaborative_filtering.py
@@ -13,7 +13,7 @@ if sys.argv[1] is None:
     exit()
 
 mongo_collection = sys.argv[1]
-K = 5 # Number of top similar playlists to the input playlist
+K = 3 # Number of top similar playlists to the input playlist
 N = 10 # Number of recommended songs
 
 playlist_dict, unique_track_dict, playlist_ids = mongodb_communicate.get(mongo_collection)
@@ -25,23 +25,23 @@ print("Sparsity:", matrix.sparsity(playlist_track_matrix))
 r_precision_results = [] # List of tuples: (input_playlist_id, r_precision)
 for input_playlist_index, input_playlist_id in enumerate(playlist_dict.keys()):
     print("---------------------------------------------------------------")
-    print("Input playlist:", playlist_dict[input_playlist_id]['name'])
+    print("Input playlist:", playlist_dict[input_playlist_id]['name'], ": ", input_playlist_id)
 
     T, new_playlist_tracks = playlist.split(input_playlist_id, playlist_dict) # List of tracks removed from the 20%
     playlist_track_matrix = matrix.update_input_playlist_tracks(input_playlist_index, new_playlist_tracks, playlist_track_matrix, unique_track_dict)
     print("Number of tracks in 20%:", len(T))
-
     cosine_similarities = cosine_similarity.create(playlist_ids, input_playlist_id, playlist_track_matrix)
 
     top_k_most_similar_playlists = cosine_similarity.find_top_k(cosine_similarities, K)
     unique_track_scores_in_top_k = cosine_similarity.top_k_track_scores(top_k_most_similar_playlists, playlist_dict)
     recommended_tracks = cosine_similarity.recommend_n_tracks(unique_track_scores_in_top_k, N, new_playlist_tracks)
 
-    matching_tracks, r_precision = evaluation.r_precision(recommended_tracks, T, N)
+
+    matching_tracks, r_precision = evaluation.r_precision(recommended_tracks, T, N, unique_track_dict)
     r_precision_results.append((input_playlist_id, r_precision))
     print("Number of matching tracks: ", matching_tracks)
 
-    playlist_track_matrix = matrix.update_input_playlist_tracks(input_playlist_index, playlist_dict[input_playlist_id], playlist_track_matrix, unique_track_dict)
+    playlist_track_matrix = matrix.update_input_playlist_tracks(input_playlist_index, playlist_dict[input_playlist_id]["tracks"], playlist_track_matrix, unique_track_dict)
 
 print("---------------------------------------------------------------")
 

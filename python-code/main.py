1a33d35a703abff34e63d7c524e6904576f3ea5e
Quinn Averill <qsaverill@wpi.edu>
2018-10-08 16:13:24 -0400

    getting the avg of avg

--- python-code/main.py
+++ python-code/main.py
@@ -7,19 +7,42 @@ import sys
 mongo_collection = sys.argv[1]
 N = 10  # Number of songs to recommend
 
-playlist_dict, unique_track_dict, indexed_pids, indexed_tids = mongodb_communicate.get(mongo_collection)
-matrix = matrix.create(playlist_dict, unique_track_dict)
+number_of_times_to_run = 15
+iteration_r_precision_graph_data = []
+iteration_k_graph_data = []
+for i in range(number_of_times_to_run):
+    playlist_dict, unique_track_dict, indexed_pids, indexed_tids = mongodb_communicate.get(mongo_collection)
+    matrix = matrix.create(playlist_dict, unique_track_dict)
 
-graph_data = {}
-graph_data["cosine_sim"], graph_data["jaccard_sim"], uc, uj = user_based.run(playlist_dict, unique_track_dict, indexed_pids, matrix, N)
-ic, ij = item_based.run(playlist_dict, unique_track_dict, indexed_tids, matrix, N)
-graph.create_graph(graph_data, mongo_collection)
+    k_graph_data = {}
+    k_graph_data["cosine_sim"], k_graph_data["jaccard_sim"], uc, uj = user_based.run(playlist_dict, unique_track_dict, indexed_pids, matrix, N)
+    iteration_k_graph_data.append(k_graph_data)
 
-# R-precision graph
+    ic, ij = item_based.run(playlist_dict, unique_track_dict, indexed_tids, matrix, N)
+
+    # R-precision graph
+    r_precision_graph_data = {}
+    r_precision_graph_data["uc"] = uc
+    r_precision_graph_data["uj"] = uj
+    r_precision_graph_data["ic"] = ic
+    r_precision_graph_data["ij"] = ij
+    iteration_r_precision_graph_data.append(r_precision_graph_data)
+
+
 r_precision_graph_data = {}
-r_precision_graph_data["uc"] = uc
-r_precision_graph_data["uj"] = uj
-r_precision_graph_data["ic"] = ic
-r_precision_graph_data["ij"] = ij
+avg_uc = 0
+avg_uj = 0
+avg_ic = 0
+avg_ij = 0
+for iteration in iteration_r_precision_graph_data:
+    avg_uc += iteration['uc']
+    avg_uj += iteration['uj']
+    avg_ic += iteration['ic']
+    avg_ij += iteration['ij']
+r_precision_graph_data['uc'] = avg_uc / number_of_times_to_run
+r_precision_graph_data['uj'] = avg_uj / number_of_times_to_run
+r_precision_graph_data['ic'] = avg_ic / number_of_times_to_run
+r_precision_graph_data['ij'] = avg_ij / number_of_times_to_run
 
-print("---------------------------------------------------------------------------------------------------------------")
+graph.create_graph(k_graph_data, mongo_collection)
+

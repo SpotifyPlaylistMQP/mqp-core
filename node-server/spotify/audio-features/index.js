6118f9814d8288acd409efe3123ec09b67e8ad66
Quinn Averill <qsaverill@wpi.edu>
2018-11-26 14:14:39 -0500

    Added endpoint to get audio features

---
+++ node-server/spotify/audio-features/index.js
@@ -0,0 +1,28 @@
+const routes = require('express').Router();
+const request = require('request');
+
+routes.get('/', function(req, res) {
+  console.log(req.query);
+  if (req.query.tids.split(',').length > 10){
+    res.send({error: 'Cannot send over 100 Track IDs'})
+  }
+  const requestOptions = {
+    url: `https://api.spotify.com/v1/audio-features?ids=${req.query.tids}`,
+    headers: {
+      Authorization: 'Bearer ' + global.authToken
+    }
+  };
+  request.get(requestOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      res.send(body)
+    } else {
+      console.log(response.statusCode);
+      console.log(error);
+      res.send({error: 'Cannot connect to the Spotify API'})
+    }
+  });
+});
+
+console.log('GET \t/spotify/audio-features');
+
+module.exports = routes;
--- node-server/spotify/index.js
+++ node-server/spotify/index.js
@@ -1,6 +1,8 @@
 const routes = require('express').Router();
 const auth = require('./auth');
+const audioFeatures = require('./audio-features');
 
 routes.use('/auth', auth);
+routes.use('/audio-features', audioFeatures);
 
 module.exports = routes;

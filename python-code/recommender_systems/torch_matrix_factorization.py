bd02c618ba000fb1a9fee86856b88caf1b75d8e0
Quinn Averill <qsaverill@wpi.edu>
2019-01-27 12:50:06 -0500

    working on mf

--- python-code/recommender_systems/torch_matrix_factorization.py
+++ python-code/recommender_systems/torch_matrix_factorization.py
@@ -5,41 +5,29 @@ from recommender_systems.modules import helpers
 
 default_params = {
     "mpd_square_100": {
-        "alpha": 1000000,
-        "beta": 100,
-        "latent_features": 200,
+        "alpha": 0.1,
+        "beta": 1,
+        "latent_features": 30,
         "learning_rate": 1e-10,
         "percent_zeros": 0.5,
-        "optimizer": "SGD",
-        "embeddings": 0
     },
     "mpd_square_1000": {
         "alpha": 10,
         "latent_features": 5,
         "learning_rate": 1e-9,
         "percent_zeros": 0.5,
-        "optimizer": "SGD",
-        "embeddings": 0
     }
 }
 
 
 class MatrixFactorization(torch.nn.Module):
-    def __init__(self, n_users, n_items, n_factors, n_embeddings):
+    def __init__(self, n_users, n_items, n_factors):
         super().__init__()
-        self.user_factors = torch.nn.Embedding(n_users, n_factors, sparse=True)
-        self.item_factors = torch.nn.Embedding(n_items, n_factors, sparse=True)
-        if n_embeddings != 0:
-            self.user_biases = torch.nn.Embedding(n_users, n_embeddings, sparse=True)
-            self.item_biases = torch.nn.Embedding(n_items, n_embeddings, sparse=True)
+        self.user_factors = torch.nn.Embedding(n_users, n_factors)
+        self.item_factors = torch.nn.Embedding(n_items, n_factors)
 
     def forward(self, user, item):
-        try:
-            pred = self.user_biases(user) + self.item_biases(item)
-            pred += (self.user_factors(user) * self.item_factors(item)).sum(1)
-            return (self.user_factors(user) * self.item_factors(item)).sum(1)
-        except AttributeError:
-            return (self.user_factors(user) * self.item_factors(item)).sum(1)
+        return (self.user_factors(user) * self.item_factors(item)).sum(1)
 
 # https://www.ethanrosenthal.com/2017/06/20/matrix-factorization-in-pytorch/
 def get_factorized_matrix(mongo_collection, track_playlist_matrix, params=None):
@@ -47,11 +35,10 @@ def get_factorized_matrix(mongo_collection, track_playlist_matrix, params=None):
         params = default_params[mongo_collection]
 
     # Create the embeddings and "model"
-    model = MatrixFactorization(len(track_playlist_matrix[0]), len(track_playlist_matrix), params['latent_features'], params['embeddings'])
+    model = MatrixFactorization(len(track_playlist_matrix[0]), len(track_playlist_matrix), params['latent_features'])
     track_playlist_matrix = np.asarray(track_playlist_matrix).T * params["alpha"]
     loss_func = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=params["learning_rate"], weight_decay=params["beta"]) if params["optimizer"] == "SGD"\
-        else torch.optim.SparseAdam(model.parameters(), lr=params["learning_rate"])
+    optimizer = torch.optim.SGD(model.parameters(), lr=params["learning_rate"], weight_decay=params['beta'])
 
     # Train data
     # indices of rows and cols for training data

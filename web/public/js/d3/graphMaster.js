3e213f041f25ad7def627b53e1d153bca53332fb
sccoache <sam.c.coache@gmail.com>
2019-02-01 12:59:46 -0500

    added new radar chart

--- web/public/js/d3/graphMaster.js
+++ web/public/js/d3/graphMaster.js
@@ -2,8 +2,8 @@
 // When called loads two CSV files with data and creates the D3 visualizations
 // 1. Creates a table from the data ordered by K value
 // 2. Creates an interactive line graph with MF and Feature MF data
-var width = 500,
-    height = 500;
+var width = 400,
+    height = 400;
 
 // Config for the Radar chart
 var config = {
@@ -20,25 +20,27 @@ function d3_god(){
     d3.queue()
         .defer(d3.csv, '/data/mf_mpd_square_100')
         .defer(d3.csv, '/data/feature_mf_mpd_square_100')
-        .defer(d3.json, '/data/playlist_average.json')
-        .defer(d3.json, '/data/song_average.json')
-        .await(function(error, file1, file2, file3, file4) {
+        .defer(d3.json, '/data/playlist_average.json')//play_avg
+        .defer(d3.json, '/data/song_average.json')    //song_avg
+        .defer(d3.json, '/data/dataset_average.json') //data_avg
+        .await(function(error, file1, file2, play_avg, song_avg, data_avg) {
             if (error) {
                 console.error('Not again: ' + error);
             } else {
                 // console.log(file4)
-                bob_the_builder(file1, file2, file3, file4);
+                bob_the_builder(file1, file2, play_avg, song_avg, data_avg);
                 // RadarChart.draw("#chart", file3, config);
             };
     });
 };
 
 // Queue and run the D3 graphing functions
-function bob_the_builder(file1, file2, file3, file4){
+function bob_the_builder(file1, file2, play_avg, song_avg, data_avg){
     d3.queue()
         .defer(build_line_graph, file1, file2)
         .defer(build_table, file1, file2)
-        .defer(RadarChart.draw, "#chart", file3, file4, config)
+        .defer(RadarChart.draw, "#dataset_radar_chart", play_avg, data_avg, config)
+        .defer(RadarChart.draw, "#results_radar_chart", play_avg, song_avg, config)
         .await(function(error) {
             if (error) {
                 console.error('Not again: ' + error);

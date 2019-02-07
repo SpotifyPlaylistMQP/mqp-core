44942a570d45ba74cf6b76d635128db01fb45c75
Jackson <jcbaker@wpi.edu>
2019-02-07 13:17:23 -0500

    added mp3 and hover effects

--- web/server.js
+++ web/server.js
@@ -65,9 +65,42 @@ var server = http.createServer(function (req, res) {
         case '/css/images/forest.jpg':
             sendFile(res, 'public/css/images/forest.jpg', 'image/jpg')
             break
-        case '/css/audio/xoTour.mp3':
+        
+        // - - - - - AUDIO CASES - - - -
+
+        case '/css/audio/xotour.mp3':
             sendFile(res, 'public/css/audio/xoTour.mp3', 'audio/mp3')
             break
+        case '/css/audio/modelz.mp3':
+            sendFile(res, 'public/css/audio/modelz.mp3', 'audio/mp3')
+            break
+        case '/css/audio/chill.mp3':
+            sendFile(res, 'public/css/audio/chill.mp3', 'audio/mp3')
+            break
+        case '/css/audio/x.mp3':
+            sendFile(res, 'public/css/audio/x.mp3', 'audio/mp3')
+            break
+        case '/css/audio/youwasright.mp3':
+            sendFile(res, 'public/css/audio/youwasright.mp3', 'audio/mp3')
+            break
+        case '/css/audio/moneylonger.mp3':
+            sendFile(res, 'public/css/audio/moneylonger.mp3', 'audio/mp3')
+            break
+        case '/css/audio/trapqueen.mp3':
+            sendFile(res, 'public/css/audio/trapqueen.mp3', 'audio/mp3')
+            break
+        case '/css/audio/unforgettable.mp3':
+            sendFile(res, 'public/css/audio/unforgettable.mp3', 'audio/mp3')
+            break
+        case '/css/audio/swang.mp3':
+            sendFile(res, 'public/css/audio/swang.mp3', 'audio/mp3')
+            break
+        case '/css/audio/jumpman.mp3':
+            sendFile(res, 'public/css/audio/jumpman.mp3', 'audio/mp3')
+            break
+        // - - - - - END AUDIO CASES - - - - -
+
+        
         // - - - - - JS Cases - - - - -
         /*
             Section 1: D3 JS files

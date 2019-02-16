fac53240e83269183286c74cf192418855f2ddc1
Quinn Averill <qsaverill@wpi.edu>
2019-02-16 14:56:33 -0500

    got torch running

--- python-code/recommender_systems/torch_matrix_factorization.py
+++ python-code/recommender_systems/torch_matrix_factorization.py
@@ -6,20 +6,14 @@ from torch.autograd import Variable
 default_params = {
     "mpd_square_100": {
         "alpha": 10000,
-        "regularization": 1,
         "latent_features": 100,
         "steps": 500,
-        "error_limit": 1e-6,
-        "fit_error_limit": 1e-5,
         "learning_rate": 1e-7
     },
     "mpd_square_1000": {
         "alpha": 10000,
-        "regularization": 100,
         "latent_features": 100,
-        "steps": 300,
-        "error_limit": 1e-6,
-        "fit_error_limit": 1e-5,
+        "steps": 500,
         "learning_rate": 1e-7
     }
 }
@@ -27,24 +21,21 @@ default_params = {
 def get_factorized_matrix(mongo_collection, track_playlist_matrix, params=None):
     if params is None:
         params = default_params[mongo_collection]
-    track_playlist_matrix = torch.Tensor(track_playlist_matrix) * params["alpha"]
 
     # initial matrices. item_features is random [0,1] and user_features is item_features\X.
-    items, users = track_playlist_matrix.shape
-    item_features = Variable(torch.rand(items, params['latent_features']), requires_grad=True)
-    user_features = Variable(torch.rand(users, params['latent_features']), requires_grad=True)
+    track_playlist_matrix = torch.Tensor(track_playlist_matrix) * params["alpha"]
+    item_features = torch.rand(len(track_playlist_matrix), params['latent_features'], requires_grad=True)
+    user_features = torch.rand(len(track_playlist_matrix[0]), params['latent_features'], requires_grad=True)
 
     # Alternating Least Squares
     for i in range(1, params['steps'] + 1):
-        # Fix item features
         loss = (track_playlist_matrix - torch.mm(item_features, torch.t(user_features))).pow(2).sum()
         loss.backward()
-        print(item_features.grad)
-        item_features = Variable(item_features - params["learning_rate"] * item_features.grad, requires_grad=True)
-        user_features = user_features - params["learning_rate"] * user_features.grad
-        print(item_features.grad)
-        item_features.grad.zero_()
-        user_features.grad.zero_()
+        with torch.no_grad():
+            item_features -= params["learning_rate"] * item_features.grad
+            user_features -= params["learning_rate"] * user_features.grad
+            item_features.grad.zero_()
+            user_features.grad.zero_()
 
     return torch.t(torch.mm(item_features, torch.t(user_features))).tolist()
 

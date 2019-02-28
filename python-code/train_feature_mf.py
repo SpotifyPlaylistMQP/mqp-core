6bc21a48f21dd4987aeb302d9e312c6886f7bf2f
Quinn Averill <qsaverill@wpi.edu>
2019-02-28 17:11:43 -0500

    final results for 100

--- python-code/train_feature_mf.py
+++ python-code/train_feature_mf.py
@@ -1,5 +1,5 @@
 import sys
-from recommender_systems import feature_matrix_factorization
+from recommender_systems import matrix_factorization
 from recommender_systems.modules import matrix, helpers, evaluation
 from mongodb import mongodb_communicate
 import time
@@ -7,20 +7,20 @@ import time
 all_params = {
     "mpd_square_100": {
         "number_of_runs": 100,
-        "alpha_set": [10000],
-        "beta_set": [1],
-        "latent_features_set": [1],
-        "steps_set": [1],
-        "c_set": [1e2, 1e1, 1, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 1e-14],
+        "alpha_set": [1e4],
+        "latent_features_set": [300],
+        "steps_set": [250],
+        "learning_rate_set": [1e-7],
+        "spotify_feature_weight_set": [1.5e-5, 1.25e-5, 1e-5, 7.5e-4, 5e-4],
         "number_of_playlists_to_test": 100,
     },
     "mpd_square_1000": {
         "number_of_runs": 10,
-        "alpha_set": [1],
-        "beta_set": [10],
-        "latent_features_set": [30],
-        "steps_set": [200],
-        "c_set": [1e-1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8, 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 0],
+        "alpha_set": [10000],
+        "latent_features_set": [1],
+        "steps_set": [1],
+        "learning_rate_set": [],
+        "c_set": [ 1e-9, 1e-10, 1e-11, 1e-12, 1e-13, 1e-14],
         "number_of_playlists_to_test": 1000,
     }
 }
@@ -31,7 +31,7 @@ params = all_params[mongo_collection]
 filename = "feature_mf_" + mongo_collection + ".txt"
 open("train_data/" + filename, "w").close()
 output = open("train_data/" + filename, "a")
-output.write("alpha, beta, latent_features, steps, C, NDCG\n")
+output.write("alpha, latent_features, steps, learning_rate, C, NDCG\n")
 
 playlist_dict, unique_track_dict, indexed_pids, indexed_tids = mongodb_communicate.get(mongo_collection)
 track_playlist_matrix = matrix.create(playlist_dict, unique_track_dict)
@@ -39,19 +39,16 @@ track_playlist_matrix = matrix.create(playlist_dict, unique_track_dict)
 N = 10
 
 for alpha in params["alpha_set"]:
-    for beta in params["beta_set"]:
-        for latent_features in params["latent_features_set"]:
-            for steps in params["steps_set"]:
-                for c in params["c_set"]:
+    for latent_features in params["latent_features_set"]:
+        for steps in params["steps_set"]:
+            for learning_rate in params["learning_rate_set"]:
+                for spotify_feature_weight in params["spotify_feature_weight_set"]:
                     train_params = {
-                        "alpha": 10000,
-                        "regularization": 1,
-                        "latent_features": 100,
-                        "steps": 500,
-                        "error_limit": 1e-6,
-                        "fit_error_limit": 1e-5,
-                        "learning_rate": 1e-7,
-                        "c": c
+                        "alpha": alpha,
+                        "latent_features": latent_features,
+                        "steps": steps,
+                        "learning_rate": learning_rate,
+                        "spotify_feature_weight": spotify_feature_weight
                     }
                     results = 0
                     for run in range(params["number_of_runs"]):
@@ -69,19 +66,19 @@ for alpha in params["alpha_set"]:
                                 unique_track_dict[tid]["energy"],
                                 unique_track_dict[tid]["valence"]
                             ])
-                        factorized_matrix = feature_matrix_factorization.get_factorized_matrix(mongo_collection, track_playlist_matrix, feature_matrix, train_params)
+                        factorized_matrix = matrix_factorization.get_factorized_matrix(mongo_collection, track_playlist_matrix, train_params, feature_matrix)
 
                         for input_playlist_index in range(params["number_of_playlists_to_test"]):
                             input_pid = indexed_pids[input_playlist_index]
 
-                            ranked_tracks = feature_matrix_factorization.get_ranked_tracks(factorized_matrix, input_playlist_index, indexed_tids)
+                            ranked_tracks = matrix_factorization.get_ranked_tracks(factorized_matrix, input_playlist_index, indexed_tids)
                             recommended_tracks = helpers.recommend_n_tracks(N, ranked_tracks, new_playlist_tracks[input_pid])
-                            results += evaluation.ndcg_precision(recommended_tracks, T[input_pid], N, unique_track_dict)
+                            results += evaluation.ndcg_precision(recommended_tracks, T[input_pid])
 
                             matrix.update_input_playlist_tracks(input_playlist_index, new_playlist_tracks[input_pid] + T[input_pid], track_playlist_matrix, unique_track_dict)
 
                     avg_ndcg = results / (params['number_of_playlists_to_test'] * params["number_of_runs"])
-                    print("{}, {}, {}, {}, {}, NDCG:{}".format(alpha, beta, latent_features, steps, c, avg_ndcg))
-                    output.write("{}, {}, {}, {}, {}, {}\n".format(alpha, beta, latent_features, steps, c, avg_ndcg))
+                    print("{}, {}, {}, {}, {}, NDCG:{}".format(alpha, latent_features, steps, learning_rate, spotify_feature_weight, avg_ndcg))
+                    output.write("{}, {}, {}, {}, {}, {}\n".format(alpha, latent_features, steps, learning_rate, spotify_feature_weight, avg_ndcg))
 
 print("Wrote results to train_data/" + filename)
